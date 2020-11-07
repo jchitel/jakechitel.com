81bb2e61f4187051336ef53656dc09e6b4591b53
Jake Chitel <jchitel@gmail.com>
2020-11-06 22:44:00 -0600

    updated everything, added typescript and styled-components

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    html,
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+`;
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Jake Chitel</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1>Jake Chitel</h1>
+            <h2>Software Engineer</h2>
+        </div>
+    )
+}
