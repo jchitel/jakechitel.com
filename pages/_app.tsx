76db642cf6484ebdf05c29c4999c33542322b04a
Jake Chitel <jchitel@gmail.com>
2020-12-24 23:27:24 -0600

    moved copy to a separate file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { GlobalStyle } from '../styles/global';
 
+// https://developers.google.com/fonts/docs/css2
+// This is requesting weights 200-900 for both non-italic and italic.
 let sansSerif = 'family=Jost:ital,wght@0,200..900;1,200..900';
 let serif = 'family=Bitter';
 let monospace = 'family=Source+Code+Pro'
